3d97d18a9fad2528bc8cd36e78226598a0e9d6fa
Jose Espejo <joseespejo@yahoo.com>
2024-01-13 17:02:07 +0000

    feat: add feed fetch

--- client/App.tsx
+++ client/App.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import FeedList from "./components/FeedList";
 
 function App() {
+  const [feedData, setFeedData] = useState([]);
 
+  useEffect(() => {
+    fetch("http://localhost:4000/feed")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+
+        setFeedData(data);
+      });
+  }, []);
 
   return (
     <div className="App">
+      <FeedList data={feedData} />
     </div>
   );
 }
