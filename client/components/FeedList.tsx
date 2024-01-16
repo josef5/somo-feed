c17ca2458a73c7f0875349d09dccff861d483c4c
Jose Espejo <joseespejo@yahoo.com>
2024-01-16 19:56:18 +0000

    feat: make feed list clickable

--- client/components/FeedList.tsx
+++ client/components/FeedList.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
-// import CardContent from "@mui/material/CardContent";
-import { Avatar, CardHeader, CardMedia, Typography } from "@mui/material";
+import { Avatar, Box, CardHeader, CardMedia, Typography } from "@mui/material";
+import { useModal } from "./ModalContext";
 
 interface FeedItem {
   briefref: string;
@@ -22,44 +22,55 @@ interface FeedItem {
 
 const FeedList: React.FunctionComponent<{ data: FeedItem[] }> = (props) => {
   const { data } = props;
-  // console.log("data :", data);
+  const { openModal } = useModal();
 
   return (
     <>
       {data.map((item, index) => (
-        <Card sx={{ width: 345, position: "relative" }} key={index}>
+        <Card
+          // onClick={() => openModal({ feedItem: item })}
+          sx={{ width: 345, position: "relative" }}
+          key={index}
+        >
           <CardHeader
             avatar={<Avatar src={item.brand.logo} aria-label="logo"></Avatar>}
-            action={<Button>Join brief now</Button>}
+            action={
+              <Button onClick={() => openModal({ feedItem: item })}>
+                Join brief now
+              </Button>
+            }
             title={item.brand.name}
           />
-          <CardMedia
-            component="img"
-            height="194"
-            image={item.banner_image}
-            alt={item.feed_title}
-          />
-          <Typography
-            variant="h5"
-            component="div"
-            sx={{
-              position: "absolute",
-              bottom: 0,
-              left: 0,
-              textShadow:
-                "1px 1px 2px rgba(0, 0, 0, 1), 1px 1px 4px rgba(0, 0, 0, 1)",
-              color: "white",
-              padding: "8px",
-            }}
+          <Box
+            onClick={() => openModal({ feedItem: item })}
+            sx={{ cursor: "pointer" }}
           >
-            {item.feed_title}
-          </Typography>
-          {/* <pre>{JSON.stringify(item, null, 2)}</pre> */}
+            <CardMedia
+              component="img"
+              height="200"
+              image={item.banner_image}
+              alt={item.feed_title}
+            />
+            <Typography
+              variant="h5"
+              component="div"
+              sx={{
+                position: "absolute",
+                bottom: 0,
+                left: 0,
+                textShadow:
+                  "1px 1px 2px rgba(0, 0, 0, 1), 1px 1px 4px rgba(0, 0, 0, 1)",
+                color: "white",
+                padding: "8px",
+              }}
+            >
+              {item.feed_title}
+            </Typography>
+          </Box>
         </Card>
       ))}
     </>
   );
-  // return <pre>{JSON.stringify(feedData, null, 2)}</pre>;
 };
 
 export default FeedList;
